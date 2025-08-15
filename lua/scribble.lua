a477652957d02ecd11ae264fc980b686feae41a7
AnkushRoy-code <ankush3411111@gmail.com>
2025-08-15 17:45:32 +0530

    made the window function modular removed magic numbers

--- lua/scribble.lua
+++ lua/scribble.lua
@@ -4,23 +4,34 @@ M.setup = function()
 	-- nothing rn
 end
 
-local open_floating_window = function()
-	local buf = vim.api.nvim_create_buf(false, false)
+local function open_floating_window(opts)
+	opts = opts or {}
 
-	local width = math.floor(vim.o.columns / 2)
-	local height = math.floor(vim.o.lines / 2)
+	local listed = false
+	local scratch = false
 
-	local opts = {
+	local buf = vim.api.nvim_create_buf(listed, scratch)
+
+	local width = opts.width or math.floor(vim.o.columns / 2)
+	local height = opts.height or math.floor(vim.o.lines / 2)
+
+	local col = opts.col or math.floor(vim.o.columns / 4)
+	local row = opts.row or math.floor(vim.o.lines / 4)
+
+	local winopts = {
 		relative = "win",
 		width = width,
 		height = height,
-		col = width / 2,
-		row = height / 2,
-		style = "minimal",
-		border = "rounded",
+		col = col,
+		row = row,
+		style = opts.style or "minimal",
+		border = opts.border or "rounded",
+		title = "Scribble",
+		title_pos = "left"
 	}
 
-	local win = vim.api.nvim_open_win(buf, true, opts)
+	local focus_the_window = true
+	local win = vim.api.nvim_open_win(buf, focus_the_window, winopts)
 
 	return win
 end
