bad12aefc53d829b38488c4c0246fb6ec2f0e301
AnkushRoy-code <ankush3411111@gmail.com>
2025-08-15 18:47:31 +0530

    added global state
    calculation of width/height to center it
    keybind and usercmd also addded

--- lua/scribble.lua
+++ lua/scribble.lua
@@ -1,5 +1,12 @@
 local M = {}
 
+local state = {
+	floating = {
+		buf = -1,
+		win = -1,
+	},
+}
+
 M.setup = function()
 	-- nothing rn
 end
@@ -7,16 +14,21 @@ end
 local function open_floating_window(opts)
 	opts = opts or {}
 
-	local listed = false
+	local listed = true
 	local scratch = false
 
-	local buf = vim.api.nvim_create_buf(listed, scratch)
+	if not vim.api.nvim_buf_is_valid(state.floating.buf) then
+		state.floating.buf = vim.api.nvim_create_buf(listed, scratch)
+	end
 
-	local width = opts.width or math.floor(vim.o.columns / 2)
-	local height = opts.height or math.floor(vim.o.lines / 2)
+	local term_width = vim.o.columns
+	local term_height = vim.o.lines
 
-	local col = opts.col or math.floor(vim.o.columns / 4)
-	local row = opts.row or math.floor(vim.o.lines / 4)
+	local width = opts.width or math.floor(term_width * 0.75)
+	local height = opts.height or math.floor(term_height / 2)
+
+	local col = opts.col or (term_width - width) / 2
+	local row = opts.row or (term_height - height) / 4
 
 	local winopts = {
 		relative = "win",
@@ -27,15 +39,25 @@ local function open_floating_window(opts)
 		style = opts.style or "minimal",
 		border = opts.border or "rounded",
 		title = "Scribble",
-		title_pos = "left"
+		title_pos = "center",
 	}
 
 	local focus_the_window = true
-	local win = vim.api.nvim_open_win(buf, focus_the_window, winopts)
+
+	local win = vim.api.nvim_open_win(state.floating.buf, focus_the_window, winopts)
 
 	return win
 end
 
-open_floating_window()
+local toggle_scribble = function()
+	if not vim.api.nvim_win_is_valid(state.floating.win) then
+		state.floating.win = open_floating_window()
+	else
+		vim.api.nvim_win_hide(state.floating.win)
+	end
+end
+
+vim.api.nvim_create_user_command("Scribble", toggle_scribble, {})
+vim.keymap.set("n", "<leader>sc", toggle_scribble)
 
 return M
